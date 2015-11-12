e98a216405a15f66ce9c53f80919d9ff74ad31eb
Mrunal Brahmbhatt <mrunal.brahmbhatt@rxpservices.com>
2015-11-12 11:07:47 +1100

    Fix path bug for special characters and update package

--- SharedSource.ContextPackageGenerator/ExportAsPackage.cs
+++ SharedSource.ContextPackageGenerator/ExportAsPackage.cs
@@ -1,74 +1,78 @@
 ﻿using Sitecore.Data.Items;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using SC = Sitecore;
 namespace Sitecore.SharedSource.Command.Export
 {
-	[Serializable]
-	public class ExportAsPackage : SC.Shell.Framework.Commands.Command
-	{
-		public override void Execute(SC.Shell.Framework.Commands.CommandContext context)
-		{
-			if ((context.Items == null) || (context.Items.Length != 1))
-				return;
+    [Serializable]
+    public class ExportAsPackage : SC.Shell.Framework.Commands.Command
+    {
+        public override void Execute(SC.Shell.Framework.Commands.CommandContext context)
+        {
+            if ((context.Items == null) || (context.Items.Length != 1))
+                return;
 
-			string currentUserName = Sitecore.Context.User.Profile.FullName; //Current User Full name.
+            string currentUserName = Sitecore.Context.User.Profile.FullName; //Current User Full name.
 
-			//Admin Account
-			Sitecore.Security.Accounts.User scUser = Sitecore.Security.Accounts.User.FromName("sitecore\\admin", false);
-			using (new Sitecore.Security.Accounts.UserSwitcher(scUser))
-			{
-				Sitecore.Data.Database db = Context.ContentDatabase;
-				Sitecore.Install.PackageProject document = new Sitecore.Install.PackageProject();
+            //Admin Account
+            Sitecore.Security.Accounts.User scUser = Sitecore.Security.Accounts.User.FromName("sitecore\\admin", false);
+            using (new Sitecore.Security.Accounts.UserSwitcher(scUser))
+            {
+                Sitecore.Data.Database db = Context.ContentDatabase;
+                Sitecore.Install.PackageProject document = new Sitecore.Install.PackageProject();
 
-				document.Metadata.PackageName = context.Items[0].Name;
-				document.Metadata.Author = currentUserName;
+                document.Metadata.PackageName = context.Items[0].Name;
+                document.Metadata.Author = currentUserName;
 
-				Sitecore.Install.Items.ExplicitItemSource source = new Sitecore.Install.Items.ExplicitItemSource();
-				source.Name = context.Items[0].Name;
+                Sitecore.Install.Items.ExplicitItemSource source = new Sitecore.Install.Items.ExplicitItemSource();
+                source.Name = context.Items[0].Name;
 
 
-				List<Item> items = new List<Item>();
+                List<Item> items = new List<Item>();
 
-				items.Add(db.Items.Database.GetItem(context.Items[0].Paths.Path)); //Self Item
-				var children = db.Items.Database.SelectItems(context.Items[0].Paths.Path + "//*");
-				if (children != null && children.Length > 0)
-					items.AddRange(children); //Get children.
+                items.Add(db.Items.Database.GetItem(context.Items[0].Paths.Path)); //Self Item
+                //decorate item name with # otherwise query will break because of special character.
+                var paths = SC.StringUtil.Split(context.Items[0].Paths.Path, '/', true).Where(p => p != null & p != string.Empty).Select(p => "#" + p + "#").ToList();
+                string allChildQuery = string.Format("/{0}//*", SC.StringUtil.Join(paths, "/"));
+                var children = db.Items.Database.SelectItems(allChildQuery);
+                if (children != null && children.Length > 0)
+                    items.AddRange(children); //Get children.
 
-				foreach (Sitecore.Data.Items.Item item in items)
-				{
-					source.Entries.Add(new Sitecore.Install.Items.ItemReference(item.Uri, false).ToString());
-				}
+                foreach (Sitecore.Data.Items.Item item in items)
+                {
+                    source.Entries.Add(new Sitecore.Install.Items.ItemReference(item.Uri, false).ToString());
+                }
 
-				document.Sources.Add(source);
-				document.SaveProject = true;
-				// Path where the zip file package will be saved
-				string filePath = Sitecore.Configuration.Settings.DataFolder + "/packages/" + context.Items[0].Name + "_" + DateTime.Now.ToString("dd_MM_yyyy_hh_mm_ss_fffffff") + ".zip";
+                document.Sources.Add(source);
+                document.SaveProject = true;
+                // Path where the zip file package will be saved
+                string filePath = Sitecore.Configuration.Settings.DataFolder + "/packages/" + context.Items[0].Name + "_" + DateTime.Now.ToString("dd_MM_yyyy_hh_mm_ss_fffffff") + ".zip";
 
-				using (Sitecore.Install.Zip.PackageWriter writer = new Sitecore.Install.Zip.PackageWriter(filePath))
-				{
-					Sitecore.Context.SetActiveSite("shell");
+                using (Sitecore.Install.Zip.PackageWriter writer = new Sitecore.Install.Zip.PackageWriter(filePath))
+                {
+                    Sitecore.Context.SetActiveSite("shell");
 
-					writer.Initialize(Sitecore.Install.Installer.CreateInstallationContext());
+                    writer.Initialize(Sitecore.Install.Installer.CreateInstallationContext());
 
-					Sitecore.Install.PackageGenerator.GeneratePackage(document, writer);
+                    Sitecore.Install.PackageGenerator.GeneratePackage(document, writer);
 
-					Sitecore.Context.SetActiveSite("website");
-				}
+                    Sitecore.Context.SetActiveSite("website");
+                }
 
-				Sitecore.Web.UI.Sheer.SheerResponse.Download(filePath);
+                Sitecore.Web.UI.Sheer.SheerResponse.Download(filePath);
 
-			}
-		}
-		public override Shell.Framework.Commands.CommandState QueryState(Shell.Framework.Commands.CommandContext context)
-		{
-			if ((context.Items == null) || (context.Items.Length != 1))
-				return Shell.Framework.Commands.CommandState.Hidden;
-			else
-				// if single item selected.
-				return base.QueryState(context);
-		}
-	}
+            }
+        }
+        public override Shell.Framework.Commands.CommandState QueryState(Shell.Framework.Commands.CommandContext context)
+        {
+            if ((context.Items == null) || (context.Items.Length != 1))
+                return Shell.Framework.Commands.CommandState.Hidden;
+            else
+                // if single item selected.
+                return base.QueryState(context);
+        }
+    }
 
 
 }
